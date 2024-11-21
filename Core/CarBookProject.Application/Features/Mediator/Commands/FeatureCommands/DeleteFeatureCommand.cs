ea7994dfb059c23beff43b8c16e7fee79868ea26
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-11-22 02:28:27 +0300

    MediatR tasarım deseni üzerinden listeleme için Handle metodu yazıldı. NOT:  CQRSten temel farkı Queries içerisine Listeleme metodu için ekstra bir Query sınıfı oluşturmak ve bu sınıfı IRequest sınıfından miras aldırarak parametre olarak liste şeklinde result sınıfını göndermek. Handler sınıfında da IRequestHandle sınıfından miras alarak Query sınıfıyla Result sınıfı handler içerisine dahil edilip imlement interface yapılıyor ve ilgili metot yine aynı şekilde yazılıyor.

---
+++ Core/CarBookProject.Application/Features/Mediator/Commands/FeatureCommands/DeleteFeatureCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookProject.Application.Features.Mediator.Commands.FeatureCommands
+{
+    public class DeleteFeatureCommand
+    {
+public int Id { get; set; }
+
+        public DeleteFeatureCommand(int id)
+        {
+            Id = id;
+        }
+    }
+}
