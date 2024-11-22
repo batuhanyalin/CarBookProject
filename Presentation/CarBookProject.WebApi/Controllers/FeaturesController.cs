e5ff91a740f919b6082302a053b36287eca4e64a
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-11-22 03:56:23 +0300

    Mediator tasarım desenine ait FeatureAPI oluşturuldu.

---
+++ Presentation/CarBookProject.WebApi/Controllers/FeaturesController.cs
@@ -0,0 +1,53 @@
+﻿using CarBookProject.Application.Features.Mediator.Commands.FeatureCommands;
+using CarBookProject.Application.Features.Mediator.Handlers.FeatureHandlers;
+using CarBookProject.Application.Features.Mediator.Queries.FeatureQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace CarBookProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public FeaturesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> ListFeature()
+        {
+            var values = _mediator.Send(new GetFeatureQuery()); //Send Metodu handlerlara istekte bulunmayı sağlıyor.
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFeature(int id)
+        {
+            var value = await _mediator.Send(new GetFeatureByIdQuery(id));
+            return Ok(value);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateFeature(CreateFeatureCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateFeature(UpdateFeatureCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteFeature(DeleteFeatureCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+    }
+
+}
