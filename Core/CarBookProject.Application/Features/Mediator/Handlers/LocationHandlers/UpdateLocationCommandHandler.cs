be3a5b1c6955a1e35988a69c1742c739389bd2f3
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-11-22 07:20:23 +0300

    Mediator tasarım deseni için gerekli sınıflar oluşturuldu ve API lar kuruldu.

---
+++ Core/CarBookProject.Application/Features/Mediator/Handlers/LocationHandlers/UpdateLocationCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using CarBookProject.Application.Interfaces;
+using CarBookProject.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookProject.Application.Features.Mediator.Handlers.LocationHandlers
+{
+    public class UpdateLocationCommandHandler
+    {
+        private readonly IRepository<Location> _repository;
+
+        public UpdateLocationCommandHandler(IRepository<Location> repository)
+        {
+            _repository = repository;
+        }
+    }
+}
