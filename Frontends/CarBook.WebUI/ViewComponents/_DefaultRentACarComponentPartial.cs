521732ab6ea53d01ffb6972b0d6b845647933c02
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-11-23 05:33:44 +0300

    Bütün UI template viewComponentlere ayrıldı ve DefaultView tamamen viewComponentlere bölündü.

---
+++ Frontends/CarBook.WebUI/ViewComponents/_DefaultRentACarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.ViewComponents
+{
+    public class _DefaultRentACarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
