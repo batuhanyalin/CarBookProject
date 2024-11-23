7db4eb272b4eeaf10be517a47389a5ad2117ec86
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-11-23 06:01:13 +0300

    Template sayfaları controllerlara bölündü ve düzenlendi.

---
+++ Frontends/CarBook.WebUI/Controllers/CarsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class CarsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CarDetail()
+        {
+            return View();
+        }
+    }
+}
